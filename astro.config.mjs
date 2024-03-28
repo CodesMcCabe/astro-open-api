23589cae7ad8c8307ba5cc1b909a673a308f2a12
Cody McCabe <cmccabe524@gmail.com>
2024-03-28 16:12:34 -0400

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+import mdx from "@astrojs/mdx";
+import openapiBackend from "@astro-openapi/backend";
+import openapiTypegen from "@astro-openapi/typegen";
+import openapiBundler from "@astro-openapi/bundler";
+import openapiClient from "@astro-openapi/client";
+
+import openapiGuiSwagger from "@astro-openapi/gui-swagger";
+import openapiGuiElements from "@astro-openapi/gui-elements";
+import openapiGuiRedoc from "@astro-openapi/gui-redoc";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react(), mdx(), openapiBackend(), openapiTypegen(), openapiBundler(), openapiClient(), openapiGuiSwagger(), openapiGuiElements(), openapiGuiRedoc()]
+});
